0260796994d23e4c74593e80ea533d742ce5ef26
Mariusz <dammariusz@o2.pl>
2018-08-22 22:28:03 +0100

    config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -100,7 +100,7 @@ Rails.application.configure do
     :port                 => '587',
     :authentication       => :plain,
     :user_name            => ENV['SENDGRID_USERNAME'],
-    :password             => ENV['SENDGIR_PASSWORD'],
+    :password             => ENV['SENDGIRD_PASSWORD'],
     :domain               => 'heroku.com',
     :enable_starttls_auto => true
     }
