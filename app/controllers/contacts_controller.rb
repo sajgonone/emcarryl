248316a8eb39acb828921f8afc83a50185a48ffd
Mariusz <dammariusz@o2.pl>
2018-08-24 10:23:08 +0100

    contacts_controller.rb

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -6,6 +6,7 @@ class ContactsController < ApplicationController
     def create
         @contact = Contact.new(params[:contact])
         @contact.request = request
+        
         if @contact.deliver
             flash.now[:error] = nil
         else
